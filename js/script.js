44af9449ad16f151ec713c88f664e3e70221d81b
awkale <hi@awkale.me>
2013-10-06 12:56:41 -0400

    first attempt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
 	/* your code goes here */
+
+	for (var i=0; i<27; i++){
+		var j  = listings[i%3]; //multiplying the 3 elements in the array to 27
+		listings.push(j);
+	}
+	console.log(listings);
+
+	var markup = "";
+
+	for (var i=0; i < listings.length; i++){
+		markup += '<div class="listing"><div class="photo-container"><img src="'+listings[i].image+'"/></div>';
+		markup +=
+	}
+
+	$('#listing-container').html(markup);
+
+
+	var page = 0;
+
+
+	$('#next').click(function(e){
+		e.preventDefault();
+		var numItems = $('#num-items').val();
+		var first_el = page * numItems;
+		for(var i = first_el; i < first_el + numItems; i++){
+			markup += '<div class="listing"><div class="photo-container"><img src="'+listings[i].image+'"/></div>';
+		markup +=
+
+		}
+	});
+
+
+
+
 	var $slides = $('#slides').find('li');
 	var slideCount = $slides.length;
 	var nextSlideIndex = 0;
@@ -27,25 +61,25 @@ $(document).ready(function(){
 	// Listings page code
 
 	var listings = [
-		{ 
+		{
 			address: "123 41st St",
 			bedrooms: 2,
 			bathrooms: 2,
 			image: "img/1bdrm_a.jpg"
 		},
-		{ 
+		{
 			address: "234 52nd St",
 			bedrooms: 1,
 			bathrooms: 1,
 			image: "img/1bdrm_b.jpg"
 		},
-		{ 
+		{
 			address: "345 63rd St",
 			bedrooms: 3,
 			bathrooms: 2,
 			image: "img/1bdrm_c.jpg"
 		}
 	];
-	
+
 
 });
